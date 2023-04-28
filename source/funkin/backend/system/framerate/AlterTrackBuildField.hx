27fc7232655aa2c5441406c173848adb382f34d8
AKRGames <65655953+AKRGames@users.noreply.github.com>
2023-04-28 21:54:09 +0200

    i funkin' forgor :skull:

--- source/funkin/backend/system/framerate/AlterTrackBuildField.hx
+++ source/funkin/backend/system/framerate/AlterTrackBuildField.hx
@@ -4,6 +4,7 @@ import openfl.text.TextFormat;
 import openfl.display.Sprite;
 import openfl.text.TextField;
 import funkin.backend.system.macros.GitCommitMacro;
+import lime.app.Application;
 
 class AlterTrackBuildField extends TextField {
 	public function new() {
@@ -11,6 +12,6 @@ class AlterTrackBuildField extends TextField {
 		defaultTextFormat = Framerate.textFormat;
 		autoSize = LEFT;
 		multiline = wordWrap = false;
-		text = 'AlterTrack v0.0.0\nBuild ${funkin.backend.system.macros.AlterBuildCounterMacro.getATBuildNumber()}';
+		text = 'AlterTrack v${Application.current.meta.get('version')}\nBuild ${funkin.backend.system.macros.AlterBuildCounterMacro.getATBuildNumber()}';
 	}
 }
